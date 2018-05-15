e384afc3b205781ced70d2868ae798c2c649502a
Sarath K S <sarathsivrud@gmail.com>
2018-05-15 10:55:15 +0530

    topsection component completed

--- src/components/TopSection.vue
+++ src/components/TopSection.vue
@@ -1,7 +1,8 @@
 <template>
 <div class="top-header">
-	<div>
-		<p class="top-header-title">{{title}}</p>	
+	<div class="top-wrapper">
+		<p class="top-header-title">{{title}}</p>
+		<p class="top-header-description">{{description}}</p>
 	</div>
 	
 </div>
@@ -9,27 +10,34 @@
 <script>
 export default {
   name: 'TopSection',
-  props: ['title']
+  props: ['title','description']
 };
 </script>
 
 <style scoped>
 .top-header{
-    height: 40vh;
+    height: 100vh;
     background: url("../assets/carroms.jpg");
     background-repeat: no-repeat;
     background-position: center; 
 }
 
+.top-wrapper{
+
+}
+
 .top-header-title{
 	margin: 0;
-    text-align: center;
     padding-top: 10vh;
     font-size: 100px;
     font-weight: 800;
     color: #feffe3;
     font-family: ubuntu;
     opacity: .8;
+}
+
+.top-header-description{
+
 }
 
 </style>
--- src/views/BoardGames.vue
+++ src/views/BoardGames.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
-	<top-section title="Carroms"></top-section>
+	<top-section 
+		title="Carroms" 
+		description = "A round of carrom ends when one player pockets all of his or her pieces, and the queen has been pocketed. You play rounds until one player has reached 29 points.">		
+	</top-section>
 	<div class="board-games-description">
 		<div>TABS</div>
 		<div>458645</div>
